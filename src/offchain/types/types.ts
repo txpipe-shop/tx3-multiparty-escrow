f53034118ea3ea9c2c9465e47c5e2841133e7a72
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-04-03 12:58:06 -0300

    implement type aliases for plutus types

--- src/offchain/types/types.ts
+++ src/offchain/types/types.ts
@@ -1,5 +1,13 @@
 import { Assets, OutRef } from "@spacebudz/lucid";
-import { TypesDatum } from "./plutus.ts";
+import { SingularityChannelMint, SingularityChannelSpend, TypesAction, TypesDatum } from "./plutus.ts";
+
+// Aliases for types defined in plutus generated file
+export const Channel = SingularityChannelSpend;
+export type ChannelDatum = TypesDatum;
+export type ChannelAction = TypesAction;
+export const ChannelDatumSchema = Channel.datum;
+export const ChannelRedeemerSchema = Channel.redeemer;
+export const MintRedeemerSchema = SingularityChannelMint.redeemer;
 
 export type ChannelInfo = OutRef &
-  TypesDatum & { balance: Assets; active: boolean; sender: string };
+  ChannelDatum & { balance: Assets; active: boolean; sender: string };
