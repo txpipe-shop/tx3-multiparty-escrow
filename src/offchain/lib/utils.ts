eb720e421c337624264a869cad0a57523015b5f4
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-04-03 12:59:03 -0300

    add util functions to convert to/from channel datums and redeemers

--- src/offchain/lib/utils.ts
+++ src/offchain/lib/utils.ts
@@ -1,10 +1,16 @@
-import { Blockfrost, Lucid } from "@spacebudz/lucid";
-import { env } from "../../config.ts";
+import { Data } from "@spacebudz/lucid";
+import {
+  ChannelAction,
+  ChannelDatum,
+  ChannelDatumSchema,
+  ChannelRedeemerSchema,
+} from "../types/types.ts";
 
-export const lucid = new Lucid({
-    provider: new Blockfrost(
-        env.PROVIDER_URL,
-      env.PROVIDER_PROJECT_ID,
-    ),
-    network: env.NETWORK as "Mainnet" | "Preprod"
-  });
+export const toChannelDatum = (d: ChannelDatum) =>
+  Data.to(d, ChannelDatumSchema);
+export const fromChannelDatum = (d: string) => Data.from(d, ChannelDatumSchema);
+
+export const toChannelRedeemer = (r: ChannelAction) =>
+  Data.to(r, ChannelRedeemerSchema);
+export const fromChannelRedeemer = (r: string) =>
+  Data.from(r, ChannelRedeemerSchema);
